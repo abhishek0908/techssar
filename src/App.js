ab38f4c9a2dfbb4a1135280d309eee5584b977c3
abhishek0908 <abhishekudiya09@gmail.com>
2024-05-26 00:23:31 +0530

    added body

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 
 import React from 'react';
 import Header from './components/Header';
+
+import Body from './components/Body';
 import './index.css'
 
 function App() {
     return (
         <div>
             <Header />
+            <Body/>
             {/* Other content */}
         </div>
     );
